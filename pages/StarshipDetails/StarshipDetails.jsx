6f54c350990e25e7ad612c70e0014dd3400d7606
techbyadi <pssharma.aditya@gmail.com>
2024-08-24 23:17:55 -0700

    show details of a starship

---
+++ pages/StarshipDetails/StarshipDetails.jsx
@@ -0,0 +1,30 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getStarShipDetails } from "../../services/sw-api";
+import { Link } from "react-router-dom";
+
+const StarshipDetails = () => {
+  const [starshipDetails, setStarshipDetails] = useState({})
+  const {starShipNumber} = useParams()
+
+  useEffect(()=>{
+    const fetchStarshipDetails = async () => {
+      const starshipDetails = await getStarShipDetails(starShipNumber)
+      setStarshipDetails(starshipDetails)
+    }
+    fetchStarshipDetails();
+  }, [starShipNumber])
+
+  if(!starshipDetails.length) return <h1>Loading Starship's details....</h1>
+
+  return (  
+    <div className="starships-details-container"> 
+      <div className="starship-details-card">
+        <h3> NAME: {starshipDetails.name}</h3>
+        <h3> MODEL: {starshipDetails.model}</h3>
+        <Link to = '/'><h3>Return</h3></Link>
+      </div>
+    </div>
+      )
+}
+export default StarshipDetails
